23c688f56c69a955d0c03a5b1c51376a6273eacf
Mikolaj Romanski <mikolaj.romanski@polcode.net>
2014-05-16 15:27:51 +0200

    Organizations models

---
+++ application/models/DbTable/Organizations.php
@@ -0,0 +1,7 @@
+<?php
+class zendtezst_Model_DbTable_Organizations extends Zend_Db_Table_Abstract
+{
+    protected $_name = 'organizations';
+    protected $_rowClass = 'zendtest_Model_Organizations';
+
+}
---
+++ application/models/Organizations.php
@@ -0,0 +1,7 @@
+<?php
+
+class zendtezst_Model_Organizations extends Zend_Db_Table_Abstract
+{
+    protected $_tableclass = 'zendtezst_Model_DbTable_Organizations';
+
+}

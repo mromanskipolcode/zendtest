b13153f98afeb038cfd4722327b6265971368011
Mikolaj Romanski <mikolaj.romanski@polcode.net>
2014-05-16 14:59:40 +0200

    Login window complete

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,34 +3,39 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
-        protected function _initAutoload()
-        {
-                Zend_Loader_Autoloader::getInstance()->setFallbackAutoloader(true);
+    protected function _initAutoload() 
+    {
+        Zend_Loader_Autoloader::getInstance()->setFallbackAutoloader(true);
 
-                return new Zend_Application_Module_Autoloader(
-                        array(
-                                'namespace'     => 'zendtest',
-                                'basePath'      => dirname(__FILE__)
-                                )
-                        );
-        }
-        /**
-         * Load js/css - use twitter bootstrap v3
-         */
-        protected function _initViewHelpers() {
-            $view = new Zend_View();
-            $view->headTitle('Test app project')->setSeparator(' - ');
-            
-            $view->doctype('HTML5');
-            $view->setBasePath($view->baseUrl());
-            $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
-            $view->headLink()->appendStylesheet($view->baseUrl().'/css/bootstrap.min.css');
-            $view->headLink()->appendStylesheet($view->baseUrl().'/css/page.css');
-            $view->headScript()->appendFile('https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js');
-            $view->headScript()->appendFile($view->baseUrl().'/js/bootstrap.min.js');
-            
-            
-        }
+        return new Zend_Application_Module_Autoloader(
+                array(
+            'namespace' => 'zendtest',
+            'basePath' => dirname(__FILE__)
+                )
+        );
+    }
+
+    /**
+     * Load js/css - use twitter bootstrap v3
+     */
+    protected function _initViewHelpers() 
+    {
+        $view = new Zend_View();
+        $view->headTitle('Test app project')->setSeparator(' - ');
+
+        $view->doctype('HTML5');
+        $view->setBasePath($view->baseUrl());
+        $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
+        $view->headLink()->appendStylesheet($view->baseUrl() . '/css/bootstrap.min.css');
+        $view->headLink()->appendStylesheet($view->baseUrl() . '/css/page.css');
+        $view->headScript()->appendFile('https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js');
+        $view->headScript()->appendFile($view->baseUrl() . '/js/bootstrap.min.js');
+    }
+
+    protected function _initConstants() 
+    {
+        $registry = Zend_Registry::getInstance();
+        $registry->constants = new Zend_Config($this->getApplication()->getOption('constants'));
+    }
 
 }
-
--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -2,13 +2,66 @@
 
 class LoginController extends Zend_Controller_Action
 {
-    public function indexAction(){
+
+    public function indexAction()
+    {
+
+
+
         $form = new zendtest_Form_Login();
-        $this->view->form=$form;
-        if($this->_request->getPost()){
-            $this->_forward('login');
+
+        $form->setMethod('post')->setAction(
+                $this->view->url(array('controller' => 'login', 'action' => 'login'))
+                );
+        $this->view->form = $form;
+    }
+    public function loginAction()
+    {
+        $request = $this ->getRequest();
+        // Make sure form validates first
+        $form = new zendtest_Form_Login();
+        if (!$form->isValid($request->getPost())) {
+
+            $this->_forward('index');
+            return;
+        }
+
+        $auth = Zend_Auth::getInstance();
+        $adapter = new Zend_Auth_Adapter_DbTable(
+            Zend_Db_Table_Abstract::getDefaultAdapter(),
+            'persons',
+            'email',
+            'password'
+        );
+
+        $salt = Zend_Registry::getInstance()->constants->salt;
+        
+        $adapter->setIdentity($form->getValue('username'))
+                ->setCredential(sha1($form->getValue('password').$salt));
+
+        $authResult = $auth->authenticate($adapter);
+        if ($authResult) {
+            $user = $adapter->getResultRowObject(null,'password');
+            $auth ->getStorage() ->write($user);
+            if($auth->getIdentity()->status!='A'){
+                $auth->clearIdentity();
+                $this->_redirect($this->view->url(
+                    array (
+                        'controller'     => 'login',
+                        'action'        => 'index'
+                        )
+                    ));
+            }
+            $this->_redirect(array('controller' => 'index', 'action' => 'index'));
         }
     }
-    
-    
+
+    public function logoutAction()
+    {
+        $auth = Zend_Auth::getInstance();
+        $auth->clearIdentity();
+        $this->_forward('login');
+    }
+
 }
+
